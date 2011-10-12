a9c1bbe5b615fda6d80f0c3a9147a5661e9002bb
Chad Smith <chad@nospam.me>
2011-10-12 17:30:03 -0500

    Update for OpenVBX 1.1

--- applets/direction/twiml.php
+++ applets/direction/twiml.php
@@ -4,14 +4,10 @@ $direction = 'inbound';
 if(!empty($_REQUEST['Direction']))
 	$direction = $_REQUEST['Direction'];
 
-<<<<<<< HEAD
 $response = new Response();
-=======
-$response = new TwimlResponse;
->>>>>>> feature/openvbx_1.1
 
 $next = AppletInstance::getDropZoneUrl($direction);
 if(!empty($next))
-	$response->redirect($next);
+	$response->addRedirect($next);
 
-$response->respond();
+$response->Respond();
